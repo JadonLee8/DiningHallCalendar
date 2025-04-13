0aa067cf3a4eb87ad55a0ae2963d4917805b788a
Jadon Lee <jadonlee888@gmail.com>
2025-04-13 16:53:41 -0500

    update check success

--- check_success.py
+++ check_success.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
     failed = 0
     data = load_data()
     for date, date_data in data.items():
-        if not date_data["success"]:
-            failed += 1
-        total += 1
+        for period, period_data in date_data.items():
+            for location, location_data in period_data.items():
+                if not location_data["success"]:
+                    failed += 1
+                total += 1
     print(f"Total: {total}, Failed: {failed}")
     print(f"Failed rate: {failed/total}")
 
